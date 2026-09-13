0fabc9d0114a278862e6a5eb906d9d3dc0a010ee
alexander vargach <socinskijroof@gmail.com>
2026-09-14 01:57:18 +0300

    fixing func to correctly complete the homework

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from masks import get_mask_account, get_mask_card_number
+from datetime import datetime
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info_string: str) -> str:
@@ -6,6 +7,10 @@ def mask_account_card(info_string: str) -> str:
     parts = info_string.split()
     number = parts[-1]
     name_of = " ".join(parts[:-1])
+    if not info_string or not isinstance(info_string, str):
+        return "Некорректный ввод: строка пуста"
+    if len(parts) > 3:
+        return "Некорректный ввод: лишние данные"
 
     if name_of.lower() == "счет":
         mask_number = get_mask_account(number)
@@ -16,15 +21,18 @@ def mask_account_card(info_string: str) -> str:
 
 
 print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 73654108430135874305"))
 
 
 def get_date(date_string: str) -> str:
-    """Принимает строку с датой и временем, возвращает дату в формате ДД.ММ.ГГГГ."""
-    year = date_string[0:4]  # '2003'
-    month = date_string[5:7]  # '11'
-    day = date_string[8:10]  # '21'
-
-    return f"{day}.{month}.{year}"
+    """ Принимает строку с датой и возвращает её в формате ДД.ММ.ГГГГ.
+    Использует модуль datetime для обработки любых корректных строк
+    """
+    try:
+        date_obj = datetime.fromisoformat(date_string)
+        return date_obj.strftime("%d.%m.%Y")
+    except (ValueError, TypeError):
+        return "Некорректный формат даты"
 
 
 print(get_date("2024-03-11T02:26:18.671407"))
